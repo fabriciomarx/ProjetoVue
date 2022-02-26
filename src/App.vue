2b62f065b6857a287b400a541fa48e79f93232ad
Fabricio Marques <fabricio@mundialeditora.com>
2022-02-26 19:00:18 -0300

    Lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <PrimeiroComponente />
+  <LifeCycle />
 </template>
 
 <script>
 
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import LifeCycle from './components/LifeCycle.vue'
 
   export default {
     name: 'App',
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
     }
   }
 </script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>Meu nome é: {{nome}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            nome: "Ainda não sei"
+        }
+    },
+    created(){
+        setTimeout(() => {
+            this.nome = "Fabricio"
+        }, 1000);
+    },
+    mounted(){
+        setTimeout(() => {
+            this.nome = 'Pedro'
+        }, 2000);
+    }
+}
+</script>
